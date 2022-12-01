138a8a2e916e650582c190135d4a204f8278932f
Oneh Amos <amoskhaled@gmail.com>
2022-12-01 11:24:22 +0100

    updated view

--- urlapi/views.py
+++ urlapi/views.py
@@ -1,48 +1,13 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-# from rest_framework.response import Response,redirect
-# from rest_framework.decorators import api_view
+import random
+import string
+
+from django.shortcuts import render, redirect, get_object_or_404
+
 from .models import UrlShortener
-# from .serializers import UrlShortenerSerializer
 
-import random, string
-from .forms import Url
-
-
-# Create your views here.
-
-
-# @api_view(['POST'])
-# def shortenUrl(request):
-#     data = request.data
-#     s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1'
-#     shorturl = ("".join(random.sample(s, 5)))
-#     UrlShortener.objects.create(
-#         longurl=data['longurl'],
-#         shorturl=shorturl
-#     )
-#     longurl = data['longurl']
-#     shorturl = 'http://localhost:8000' + shorturl
-
-
-# context = {
-#     'longurl': longurl,
-#     'shorturl': shorturl
-# }
-# return redirect('/')
-
-
-# def redirectUrl(request, shorturl):
-#     try:
-#         obj = UrlShortener.objects.get(shorturl=shorturl)
-#     except UrlShortener.DoesNotExist:
-#         obj = None
-#     if obj is not None:
-#         return redirect(obj.longurl)
 
 def urlShort(request):
     if request.method == 'POST':
-        # form = UrlShortener(request.POST)
         shorturl = ''.join(random.choice(string.ascii_letters)
                            for x in range(5))
         short = 'http://localhost:8000/' + shorturl
@@ -50,25 +15,16 @@ def urlShort(request):
         new_url = UrlShortener(longurl=longurl, shorturl=short)
         new_url.save()
         return render(request, 'index.html', context={'shorturl': short})
-    # else:
-    # form = Url()
     data = UrlShortener()
     context = {
-        # 'form': form,
         'data': data
     }
     return render(request, 'index.html', context)
 
 
 def urlRedirect(request, pk):
-    try:
-        data = UrlShortener.objects.get(shorturl=pk)
-    except UrlShortener.DoesNotExist:
-        return redirect('/')
-
-    if data is not None:
-        print(data.longurl)
-        return redirect('https://' + data.longurl)
+    data = get_object_or_404(UrlShortener, shorturl="http://localhost:8000/" + pk)
+    return redirect('https://' + data.longurl)
 
 
 def index(request):
--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env(BASE_DIR/ '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'urlapi.apps.UrlapiConfig',
-    'rest_framework',
     'corsheaders'
 ]
 
@@ -65,7 +63,7 @@ ROOT_URLCONF = 'urlshortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/ 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +98,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -119,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -130,7 +126,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/

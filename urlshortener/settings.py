0b5a4889b50c8347c0d98dc913934fc325a2e6d3
Oneh Amos <amoskhaled@gmail.com>
2022-12-14 23:00:09 +0100

    my updated

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://my-url.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://my-url.up.railway.app']
 
 ALLOWED_HOSTS = [env('ALLOWED_HOST')]
 

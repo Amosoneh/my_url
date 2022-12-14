8bc87842a924539a7b88bec4e413ad6bce6ca565
Oneh Amos <amoskhaled@gmail.com>
2022-12-14 22:49:06 +0100

    my updated

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
+
 import environ
 from pathlib import Path
 
@@ -130,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / '/shortener/static/']
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
